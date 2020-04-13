ad08022371f99be8606d3039ee411507cd9865e6
Stefan <stefan_silvano@yahoo.com>
2020-04-13 20:45:09 +0300

    Tema 1

---
+++ Server/HelloOperationService.cs
@@ -0,0 +1,20 @@
+﻿using Generated;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class HelloOperationService : Generated.HelloResponse.HelloResponseBase
+    {
+        public override Task<OperationResponse> SayHello(Generated.HelloRequest request, ServerCallContext context)
+        {
+            System.Console.WriteLine("Hello, " + request.Name + "!");
+            var result = "";
+            return Task.FromResult(new OperationResponse() { Message = result });
+        }
+    }
+}
